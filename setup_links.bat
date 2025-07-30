4b00a2b8bbbac68f4de58668689bffc78a3ac17f
Florian Hyver <florian@hyver.me>
2025-07-30 12:38:32 +0200

    feat: better error handling

--- setup_links.bat
+++ setup_links.bat
@@ -1,11 +1,11 @@
 @echo off
 setlocal
 
-:: Change to the directory where the script is located
-cd /d %~dp0
-
 :: Define directories to link
 set DIRS=LIVE EPTU PTU TECH-PREVIEW
+
+:: Change to the directory where the script is located
+cd /d %~dp0
 
 :: Ensure _Backup directory and its subdirectories exist
 if not exist "_Backup\Screenshots" mkdir "_Backup\Screenshots"
@@ -13,6 +13,7 @@ if not exist "_Backup\Mappings" mkdir "_Backup\Mappings"
 
 :: Iterate over each directory
 for %%d in (%DIRS%) do (
+    echo %%d
     :: Check if directory exists, if not create it
     if not exist "%%d" mkdir "%%d"
 
@@ -21,24 +22,41 @@ for %%d in (%DIRS%) do (
 
     :: Check and handle Screenshots directory
     if exist "Screenshots" (
-        echo Moving contents of Screenshots from %%d to _Backup\Screenshots...
-        xcopy "Screenshots\*" "..\_Backup\Screenshots\" /S /I /Y
-        rd /s /q "Screenshots"
+        dir | find /i "<SYMLINKD>" | find /i "Screenshots" >nul
+        if errorlevel 1 (
+            REM The folder is not a symbolic link.
+            echo Moving contents of Screenshots from %%d to _Backup\Screenshots...
+            xcopy "Screenshots\*" "..\_Backup\Screenshots\" /S /I /Y
+            rd /s /q "Screenshots"
+            echo Creating symbolic link in %%d pointing to ..\_Backup\Screenshots...
+            mklink /D "screenshots" "..\_Backup\Screenshots"
+        ) else (
+            REM The folder is a symbolic link. Do nothing
+        )
+    ) else (
+        echo Creating symbolic link in %%d pointing to ..\_Backup\Screenshots...
+        mklink /D "screenshots" "..\_Backup\Screenshots"
     )
-    echo Creating symbolic link in %%d pointing to ..\_Backup\Screenshots...
-    mklink /D "screenshots" "..\_Backup\Screenshots"
-
+    
     :: Check and handle Mappings directory
     if exist "user\client\0\Controls\Mappings" (
-        echo Moving contents of Mappings from %%d to _Backup\Mappings...
-        xcopy "user\client\0\Controls\Mappings\*" "..\_Backup\Mappings\" /S /I /Y
-        rd /s /q "user\client\0\Controls\Mappings"
+        dir "user\client\0\Controls" | find /i "<SYMLINKD>" | find /i "Mappings" >nul
+        if errorlevel 1 (
+            REM The folder is not a symbolic link.
+            echo Moving contents of Mappings from %%d to _Backup\Mappings...
+            xcopy "user\client\0\Controls\Mappings\*" "..\_Backup\Mappings\" /S /I /Y
+            rd /s /q "user\client\0\Controls\Mappings"
+            echo Creating symbolic link in %%d pointing to ..\_Backup\Mappings...
+            mklink /D "user\client\0\Controls\Mappings" "..\..\..\..\..\_Backup\Mappings"
+        ) else (
+            REM The folder is a symbolic link. Do nothing
+        )
     ) else (
         echo Creating directory structure for Mappings in %%d...
         mkdir "user\client\0\Controls"
+        echo Creating symbolic link in %%d pointing to ..\_Backup\Mappings...
+        mklink /D "user\client\0\Controls\Mappings" "..\..\..\..\..\_Backup\Mappings"
     )
-    echo Creating symbolic link in %%d pointing to ..\_Backup\Mappings...
-    mklink /D "user\client\0\Controls\Mappings" "..\..\..\..\..\_Backup\Mappings"
 
     :: Go back to the _Backup directory
     cd ".."
